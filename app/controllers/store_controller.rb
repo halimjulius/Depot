4dcd0d343c76ba2bf108f3dde2e717b813b45889
Julius Halim <halim.julius@gmail.com>
2013-04-15 11:53:35 +0700

    Added 'add to cart' from button and image, counter(end of task d)

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
+    session[:counter] ||= 0
+    session[:counter] += 1
+    if (session[:counter] > 5) then
+      @count = session[:counter]
+    else
+      @count = nil
+    end
     @products = Product.order(:title)
   end
 end
